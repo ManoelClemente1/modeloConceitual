24fa0557c3e0b0a577caed0715dd38bae16ba20d
Manoel C <manoelclemente9@outlook.com>
2020-03-24 22:44:49 -0300

    Adicionando cidade e estado

--- src/main/java/com/mc/zero/NetoApplication.java
+++ src/main/java/com/mc/zero/NetoApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.mc.zero.domain.Categoria;
+import com.mc.zero.domain.Cidade;
+import com.mc.zero.domain.Estado;
 import com.mc.zero.domain.Produto;
 import com.mc.zero.repositories.CategoriaRepository;
+import com.mc.zero.repositories.CidadeRepository;
+import com.mc.zero.repositories.EstadoRepository;
 import com.mc.zero.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -20,6 +24,12 @@ public class NetoApplication implements CommandLineRunner {
 	
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	
+	@Autowired
+	private CidadeRepository cidadeRepository;
+	
+	@Autowired
+	private EstadoRepository estadoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(NetoApplication.class, args);
@@ -30,8 +40,6 @@ public class NetoApplication implements CommandLineRunner {
 
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
-
-		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		
 		Produto p1 = new Produto(null, "Computador", 2000.00);
 		Produto p2 = new Produto(null, "Impressora", 600.00);
@@ -44,8 +52,22 @@ public class NetoApplication implements CommandLineRunner {
 		p2.getCategorias().addAll(Arrays.asList(cat1,cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
 		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
 
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "São Paulo");
+		
+		Cidade c1 = new Cidade(null, "Uberlandia", est1);
+		Cidade c2 = new Cidade(null, "São Paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+		
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2,c3));
+		
+		estadoRepository.saveAll(Arrays.asList(est1,est2));
+		cidadeRepository.saveAll(Arrays.asList(c1,c2,c3));
+		
 		
 	}
 
